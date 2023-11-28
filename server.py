89bc2131ca6f27cc43e18ce3624a944b34518792
Loay Mohamed <72279810+zoroxide@users.noreply.github.com>
2023-11-28 08:47:03 -0800

    Update server.py

--- server.py
+++ server.py
@@ -6,23 +6,23 @@ CORS(app)
 
 
 # Route to get sensor data
-@app.route('/get_sensor_data', methods=['GET'])
+@app.route('/get_data', methods=['GET'])
 def get_sensor_data():
-    return jsonify(sensor_data)
+    return jsonify(data)
 
 # Sample data storage (replace this with your actual data handling logic)
-sensor_data = {"sensor_value": 0}
+data = {"value": 0}
 
 # Route to update sensor data
-@app.route('/update_sensor_data', methods=['POST'])
-def update_sensor_data():
+@app.route('/update_data', methods=['POST'])
+def update_data():
     data = request.get_json()
 
-    if "sensor_value" in data:
-        sensor_data["sensor_value"] = data["sensor_value"]
-        return jsonify({"message": "Sensor data updated successfully"})
+    if "value" in data:
+        sensor_data["value"] = data["value"]
+        return jsonify({"message": "data updated successfully"})
     else:
-        return jsonify({"error": "Invalid request. 'sensor_value' missing."}), 400
+        return jsonify({"error": "Invalid request. 'value' missing."}), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  # Run the API server on all available network interfaces
